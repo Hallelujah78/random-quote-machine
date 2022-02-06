023bccd5b85a037820f1b876eaabbb26b930011a
Hallelujah78 <gavan.browne@gmail.com>
2022-02-06 01:29:07 +0000

    fixing tweet button

--- src/components/TweetQuote/index.js
+++ src/components/TweetQuote/index.js
@@ -3,19 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 import { Wrapper } from "./TweetQuote.styles";
 
-
-const TweetQuote = ({quote, author}) => {
-
+const TweetQuote = ({ quote, author }) => {
   return (
     <Wrapper>
-
-      <a
-        href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
-        id="tweet-quote"
-        className="twitter-share-buttons">
-
-        <FontAwesomeIcon icon={faTwitterSquare} id="twitter-quote" />
-      </a>
+      <button>
+        <a
+          href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
+          id="tweet-quote"
+          className="twitter-share-buttons"
+        >
+          <FontAwesomeIcon
+            icon={faTwitterSquare}
+            id="twitter-quote"
+            size={"20px"}
+          />
+        </a>
+      </button>
     </Wrapper>
   );
 };
