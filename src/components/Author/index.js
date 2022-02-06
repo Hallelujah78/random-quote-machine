f5eb3d8eb50d0b70a3396bffb4dfcf8f61747af7
Hallelujah78 <gavan.browne@gmail.com>
2022-02-06 01:14:28 +0000

    housekeeping

--- src/components/Author/index.js
+++ src/components/Author/index.js
@@ -3,17 +3,14 @@ import React from "react";
 //styles
 import { Wrapper, Content } from "./Author.styles.js";
 
-
-const Author = ({author}) => {
+const Author = ({ author }) => {
   return (
-  <Wrapper>
-  <Content>
-  <p id="author">{"-  " + author}</p>
-  </Content>
-  </Wrapper>
-
-  )
+    <Wrapper>
+      <Content>
+        <p id="author">{`- ${author}`}</p>
+      </Content>
+    </Wrapper>
+  );
 };
-
 
 export default Author;
